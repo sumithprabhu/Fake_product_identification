94fcac018ca9dff857af6229a5db05123f2a8b76
sumith prabhu <sumithprabhu07@gmail.com>
2023-01-01 17:12:32 +0530

    code updated

---
+++ Client/src/App.js
@@ -0,0 +1,216 @@
+import "./App.css";
+
+import React, { useEffect, useState,useRef } from "react";
+import Web3Modal from "web3modal";
+import VendorForm from "./components/VendorForm";
+import { providers, Contract } from "ethers";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DistributorForm from "./components/DistributorForm";
+import Home from "./components/Home";
+import {AssetTracker} from "./utils/AssetTracker.js";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import Products from "./components/TrackProducts";
+import Distributors from "./components/Distributors";
+
+// import SideBar from "./components/SideBar";
+
+import Authenticate from "./components/Authenticate";
+import GetStarted from "./components/getStarted";
+//require("dotenv").config({ path: ".env" });
+
+
+library.add(fas);
+
+const App = () => {
+  console.log(process.env.REACT_APP_WALLET_ADD);
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [wallet, setWallet] = useState("Please Connect Your Wallet to Proceed");
+  const [contract, setContract] = useState(null);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const web3ModalRef = useRef();
+  const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADD;
+
+  const checkIfWalletIsConnected = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Goerli network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change the network to Goerli");
+      throw new Error("Change network to Goerli");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  const connectWallet = async () => {
+    
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // When used for the first time, it prompts the user to connect their wallet
+      await checkIfWalletIsConnected();
+      setWalletConnected(true);
+      
+      
+      
+      const signer = await checkIfWalletIsConnected(true);
+      setCurrentAccount(await signer.getAddress());
+      const NContract = new Contract(
+        CONTRACT_ADDRESS,
+        AssetTracker,
+        signer
+      );
+      setContract(NContract)
+      
+    } catch (err) {
+      console.error(err);
+    }
+  };
+    // try {
+    //   const { ethereum } = window;
+
+    //   if (!ethereum) {
+    //     alert("Get MetaMask!");
+    //     return;
+    //   }
+
+    //   const accounts = await ethereum.request({
+    //     method: "eth_requestAccounts",
+    //   });
+
+    //   console.log("Connected", accounts[0]);
+
+    //   setWallet("Connected");
+
+    //   setCurrentAccount(accounts[0]);
+    //   const provider = new ethers.providers.Web3Provider(ethereum);
+    //   const signer = provider.getSigner();
+    //   const contract = new ethers.Contract(
+    //     CONTRACT_ADDRESS,
+    //     AssetTracker,
+    //     signer
+    //   );
+    //   setContract(contract);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  
+
+  useEffect(() => {
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      console.log(currentAccount)
+      connectWallet();
+    }
+  }, [walletConnected]);
+
+  return (
+    <>
+      {contract ? (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home account={currentAccount}/>}>
+              
+            </Route>
+            {/* <Route
+              path="/vendor"
+              element={<SideBar contract={contract} account={currentAccount} />}
+            ></Route> */}
+            <Route
+              path="/vendor"
+              element={
+                <GetStarted contract={contract} account={currentAccount} />
+              }
+            >
+              <Route
+                path="products"
+                element={
+                  <Products contract={contract} account={currentAccount} />
+                }
+              ></Route>
+              <Route
+                path="addproduct"
+                element={
+                  <VendorForm contract={contract} account={currentAccount} />
+                }
+              />
+              <Route
+                path="available-distributors"
+                element={
+                  <Distributors contract={contract} account={currentAccount} />
+                }
+              />
+            </Route>
+            <Route
+              path="/distributorform"
+              element={
+                <DistributorForm contract={contract} account={currentAccount} />
+              }
+            ></Route>
+            {/* <Route
+              path="/vendor/products"
+              element={
+                <Products contract={contract} account={currentAccount} />
+              }
+            ></Route>
+            <Route
+              path="/vendor/addproduct"
+              element={
+                <VendorForm contract={contract} account={currentAccount} />
+              }
+            />
+            <Route
+              path="/vendor/available-distributors"
+              element={
+                <Distributors contract={contract} account={currentAccount} />
+              }
+            /> */}
+            <Route
+              path="/authenticate"
+              element={
+                <Authenticate contract={contract} account={currentAccount} />
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        <div>
+          <div>
+            <div className="connectWalletContainer">
+              {wallet === "Please Connect Your Wallet to Proceed" && (
+                <button onClick={connectWallet} className="connectWalletBtn">
+                  <img
+                    src={
+                      "https://cdn.iconscout.com/icon/free/png-256/metamask-2728406-2261817.png"
+                    }
+                    className="img"
+                    alt="metamask"
+                  />{" "}
+                  {wallet}
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default App;
